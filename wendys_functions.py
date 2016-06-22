f8c679fb5b4b86965555e84850ff4cb362ec97f6
Wendy Grus <wendygrus@hotmail.com>
2016-06-21 22:57:12 -0700

    add notebooks

---
+++ wendys_functions.py
@@ -0,0 +1,20 @@
+def print_5times(string):
+    '''prints the input string 5 times'''
+    print(string*5)
+    
+def contains_orange(string):
+    '''takes in a sentence and returns a boolean indicating 
+    if the string contains the word orange'''
+    return "orange" in string
+    
+def print_GDI():
+    '''prints out GDI'''
+    print(' GGGG     DDDD       IIIIII')
+    print('GG  GG    DD  DD       II  ')
+    print('GG        DD   DD      II  ')
+    print('GG  GGG   DD   DD      II  ')
+    print('GG   GG   DD  DD       II  ')
+    print(' GGGGG    DDDD       IIIIII')
+    
+
+    
